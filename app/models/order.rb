aa2d072f81b65d71014f934918660f891cd55a3e
sonamrks <sonam.rks@gmail.com>
2015-06-09 21:47:25 -0700

    final checkout module completed

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,11 @@ class Order < ActiveRecord::Base
   validates :name, :address, :email, presence: true
   validates :pay_type, inclusion: PAYMENT_TYPES
 
+  def add_items_from_cart(cart)
+    cart.line_items.each do |item|
+      item.cart_id = nil
+      oid = self.id
+      item.order_id = oid
+    end
+  end
 end
